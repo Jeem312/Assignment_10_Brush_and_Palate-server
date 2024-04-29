9e53dce362ce73eacb163b3db0f5a9524a65b61d
Jeem312 <Shajidajeem312@gmail.com>
2024-04-29 23:59:17 +0600

    added MYCRAFT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://<username>:<password>@cluster0.wibgfjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const uri = `mongodb+srv://brushPalate:6CHosG6Sn0vw3EpP@cluster0.wibgfjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wibgfjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,7 +38,7 @@ async function run() {
       res.send(result);
      })
     
-     app.get('/CraftIteam',async(req,res)=>{
+     app.get('/CraftIteam', async(req,res)=>{
       const cursor = craftCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -44,7 +46,7 @@ async function run() {
       
      app.get('/mycart/:email',async(req,res)=>{
       console.log(req.params.email)
-      const result = await craftCollection.find({email:req.params.email
+      const result = await craftCollection.find({Email:req.params.email
       }).toArray();
         res.send(result)
       })
